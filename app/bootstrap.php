a018353c9c0cafd4047a4d3cdc63f93c2c431050
Jiří Svěcený <sveceny@sitole.cz>
2019-01-30 17:15:59 +0100

    Constants to upper, methods to camel case

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
-$configurator->enableTracy(__dir__ . '/../log');
-$configurator->setTempDirectory(__dir__ . '/../temp');
+$configurator->enableTracy(__DIR__ . '/../log');
+$configurator->setTempDirectory(__DIR__ . '/../temp');
 
-$configurator->addConfig(__dir__ . '/config/config.neon');
-$configurator->addConfig(__dir__ . '/config/config.local.neon');
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
 return $configurator->createContainer();
